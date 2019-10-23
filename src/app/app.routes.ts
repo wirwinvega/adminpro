3f8d6655c5537f56b448105f290e32675f4acdba
Irving <irving@bitfx.mx>
2019-10-23 15:50:10 -0500

    Initial structure complete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,10 @@
 import {RouterModule, Routes} from "@angular/router";
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register/register.component';
 
 const appRoutes: Routes = [
-    {
-        path: '', 
-        component: PagesComponent,
-        children: [
-            {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-            {path: 'dashboard', component: DashboardComponent},
-            {path: 'progress', component: ProgressComponent},
-            {path: 'graficas1', component: Graficas1Component},
-        ]
-    },
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path: '**', component: NopagefoundComponent},
